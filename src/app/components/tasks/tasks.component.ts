9b3d8a17ef1c8deb418ef0c12ca88d43ac4a942a
Sasindu1997 <sasinduusgalhewa@gmail.com>
2021-07-21 23:30:45 +0530

    creating tasks

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import {Task} from '../../Task';
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private tasksService: TaskService) { }
+
+  ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Meet the Doctor',
+        day: 'Jully 22nd at 2:30pm',
+        reminder : true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at the school',
+        day: 'Jully 23nd at 1:30pm',
+        reminder : true,
+    },
+    {
+        id: 3,
+        text: 'Go shopping',
+        day: 'Jully 24nd at 12:30pm',
+        reminder : false,
+    },
+];
